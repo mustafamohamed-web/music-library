807e4dab81725dbd21abe4e5b5046a61b349c271
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2021-09-04 19:30:51 +0100

    setting up application

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-app.use(express.json);
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('HELLO WORLD!')
+    res.status(200).send('HELLO WORLD!')
 })
 
-app.listen(4000)
+
 module.exports = app;
