036ba23ed9325ea6b7cf62faaee89db3bb534219
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2021-09-20 19:57:43 +0100

    finished

---
+++ src/controllers/artist.js
@@ -0,0 +1,97 @@
+const getDb = require('../services/db')
+exports.create = async (req, res) => {
+  
+  try {
+    const db = await getDb();
+    const { name, genre } = req.body;
+    
+
+    await db.query('INSERT INTO Artist (name, genre) VALUES (?, ?)', [
+      name,
+      genre,
+    ]);
+  
+      res.sendStatus(201);
+    } catch (err) {
+      console.log(err)
+      res.sendStatus(500).json(err);
+    }
+  
+    db.close();
+  };
+
+
+  exports.read = async (_, res) => {
+    const db = await getDb();
+  
+    try {
+      const [artists] = await db.query('SELECT * FROM Artist');
+  
+      res.status(200).json(artists);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+    db.close();
+  };
+  
+
+  exports.readById = async (req, res) => {
+    const db = await getDb();
+    const { artistId } = req.params;
+  
+    const [[artist]] = await db.query('SELECT * FROM Artist WHERE id = ?', [
+      artistId,
+    ]);
+  
+    if (!artist) {
+      res.sendStatus(404);
+    } else {
+      res.status(200).json(artist);
+    }
+  
+    db.close();
+  };
+
+  exports.update = async (req, res) => {
+    const db = await getDb();
+    const data = req.body;
+    const { artistId } = req.params;
+  
+    try {
+      const [
+        { affectedRows },
+      ] = await db.query('UPDATE Artist SET ? WHERE id = ?', [data, artistId]);
+  
+      if (!affectedRows) {
+        res.sendStatus(404);
+      } else {
+        res.status(200).send();
+      }
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  
+    db.close();
+  };
+
+
+exports.delete = async (req, res) => {
+  const db = await getDb();
+  const { artistId } = req.params;
+
+  try {
+      const [{ affectedRows }] = await db.query('DELETE FROM Artist WHERE id = ?', [
+        artistId
+      ]);
+
+    if (!affectedRows) {
+      res.sendStatus(404);
+    } else {
+      res.status(200).json(affectedRows);
+    }
+  } catch (err) {
+    res.sendStatus(500);
+  }
+
+  db.close();
+};
---
+++ src/routes/artist.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const artistController = require('../controllers/artist');
+
+const router = express.Router();
+
+router.post('/', artistController.create);
+
+router.get('/', artistController.read);
+
+router.get('/:artistId', artistController.readById);
+
+router.patch('/:artistId', artistController.update);
+
+router.delete('/:artistId', artistController.delete);
+
+module.exports = router;
